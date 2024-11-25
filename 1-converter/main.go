3cd227759f7c793b0ea4031dfed09ae41c7ff158
Dmity <dima20089133@yandex.ru>
2024-11-26 00:14:08 +0300

    Конвектор валют

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,22 +1,92 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-	const UsdEur float64 = 0.94691
-	const UsdRub float64 = 100.0
-	const EurRub float64 = UsdEur * UsdRub
+const usdEur float64 = 0.94691
+const usdRub float64 = 100.0
+const eurRub float64 = usdEur * usdRub
 
 func main() {
-	inpud := userInpud()
+	for {
+		input, err := userInput()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		numbers, err := numberInput()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		currency, err := targetCurrency(input)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		err = calculatorCurrencies(numbers, input, currency)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		} else {
+			break
+		}
+	}
 }
 
-func userInpud() (string) {
+func userInput() (string, error) {
 	var currency string
-	fmt.Print("Введите название валюты:")
+	fmt.Print("Введите название валюты (USD/EUR/RUB):")
 	fmt.Scan(&currency)
-	return currency
+	if currency == "Usd" || currency == "usd" || currency == "Eur" || currency == "eur" || currency == "Rub" || currency == "rub" {
+		return currency, nil
+	} else {
+		return "", errors.New("Такой валюты нет! Введите заново")
+	}
 }
 
-func calculateCurrencies(num int, currency1, currency2 string) (int) {
-	
+func numberInput() (float64, error) {
+	var number float64
+	fmt.Print("Введите число:")
+	fmt.Scan(&number)
+	if number > 0 {
+		return number, nil
+	} else {
+		return 0, errors.New("Такое число не существует! Введите заново")
+	}
+}
+
+func targetCurrency(currencies string) (string, error) {
+	switch {
+	case currencies == "Usd" || currencies == "usd":
+		fmt.Println("Выберете подходящею валюту (EUR, RUB)")
+		fmt.Scan(&currencies)
+	case currencies == "Eur" || currencies == "eur":
+		fmt.Println("Выберете подходящею валюту (USD, RUB)")
+		fmt.Scan(&currencies)
+	case currencies == "Rub" || currencies == "rub":
+		fmt.Println("Выберете подходящею валюту (USD, EUR)")
+		fmt.Scan(&currencies)
+	default:
+		return "", errors.New("Нет подходящий валюты")
+	}
+	return currencies, nil
+}
+
+func calculatorCurrencies(number float64, user, targetCurrency string)  error {
+	switch {
+	case user == "Usd" || user== "usd" && targetCurrency == "EUR" || targetCurrency == "eur":
+		fmt.Println(number * usdEur)
+	case user == "Usd" || user == "usd" && targetCurrency == "Rub" || targetCurrency == "rub":
+		fmt.Println(number * usdRub)
+	case user == "Eur" || user == "eur" && targetCurrency == "Rub" || targetCurrency == "rub":
+		fmt.Println(number * eurRub)
+	default:
+		return errors.New("Неправильные вычисления")
+	}
+	return nil
 }
