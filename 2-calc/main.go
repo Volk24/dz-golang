5171ef26133e72b4d6387acc19d056a213fe24c7
Dmity <dima20089133@yandex.ru>
2025-01-14 13:01:18 +0300

    DZ Продвинутые функции

--- 2-calc/main.go
+++ 2-calc/main.go
@@ -10,6 +10,12 @@ import (
 	"strings"
 )
 
+var calculator = map[string] func(string,[]int) (int,error) {
+	"AVG": avg,
+	"SUM": sum,
+	"MED": med,
+}
+
 func main() {
 	for {
 		input, err := userInputOperation()
@@ -30,7 +36,7 @@ func main() {
 			continue
 		}
 
-		calculators, err := calculator(input, operatic)
+		calculators, err := calculator[input](input, operatic)
 		if err != nil {
 			fmt.Print(err)
 			continue
@@ -78,38 +84,34 @@ func translateInt(scanner string) ([]int, error) {
 	return nums, nil
 }
 
-func calculator(userInput string, nums []int) (int, error) {
-	switch userInput {
-	case "AVG":
-		if len(nums) == 0 {
-			return 0, errors.New("Нет чисел")
-		}
-		sum := 0
-		for _, num := range nums {
-			sum += num
-		}
-		result := sum / len(nums)
-		return result, nil
+func avg(userInput string, nums []int) (int, error) {
+	if len(nums) == 0 {
+		return 0, errors.New("Некорректно набранное число")
+	}
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	result := sum / len(nums)
+	return result, nil
+}
 
-	case "SUM":
-		sum := 0
-		for _, num := range nums {
-			sum += num
-		}
-		return sum, nil
+func sum(userInput string, nums []int) (int, error) {
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	return sum, nil
+}
 
-	case "MED":
-		sort.Ints(nums)
-		result := len(nums)
-		if result%2 == 1 {
-			return nums[result/2], nil
-		} else {
-			mid1 := nums[result/2-1]
-			mid2 := nums[result/2]
-			return (mid1 + mid2) / 2, nil
-		}
-
-	default: 
-		return 0, errors.New("Ошибка в вычислениях")
+func med(userInput string, nums []int) (int, error) {
+	sort.Ints(nums)
+	result := len(nums)
+	if result%2 == 1 {
+		return nums[result/2], nil
+	} else {
+		mid1 := nums[result/2-1]
+		mid2 := nums[result/2]
+		return (mid1 + mid2) / 2, nil
 	}
 }
